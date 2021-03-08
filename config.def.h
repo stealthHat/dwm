648484774f97baa60cb3a4db3eaa4bd537a3f835
stealthhat <blinklgui@gmail.com>
2021-03-08 18:33:38 -0300

    change incons

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
